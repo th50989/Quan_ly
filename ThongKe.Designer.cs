1d892734393e9bc13b75f9f3827d7f87729555cb
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-02-20 16:00:10 +0700

    Add ThongKe Form

---
+++ ThongKe.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Phan_mem_quan_ly_bien_ban
+{
+    partial class ThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.do_an_phat_trien_phan_mem_udDataSet = new Phan_mem_quan_ly_bien_ban.do_an_phat_trien_phan_mem_udDataSet();
+            this.doanphattrienphanmemudDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.grBox_ThongKe = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.trView_ChiNhanh = new System.Windows.Forms.TreeView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.do_an_phat_trien_phan_mem_udDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doanphattrienphanmemudDataSetBindingSource)).BeginInit();
+            this.grBox_ThongKe.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // do_an_phat_trien_phan_mem_udDataSet
+            // 
+            this.do_an_phat_trien_phan_mem_udDataSet.DataSetName = "do_an_phat_trien_phan_mem_udDataSet";
+            this.do_an_phat_trien_phan_mem_udDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // doanphattrienphanmemudDataSetBindingSource
+            // 
+            this.doanphattrienphanmemudDataSetBindingSource.DataSource = this.do_an_phat_trien_phan_mem_udDataSet;
+            this.doanphattrienphanmemudDataSetBindingSource.Position = 0;
+            // 
+            // grBox_ThongKe
+            // 
+            this.grBox_ThongKe.Controls.Add(this.textBox3);
+            this.grBox_ThongKe.Controls.Add(this.label3);
+            this.grBox_ThongKe.Controls.Add(this.textBox2);
+            this.grBox_ThongKe.Controls.Add(this.label2);
+            this.grBox_ThongKe.Controls.Add(this.textBox1);
+            this.grBox_ThongKe.Controls.Add(this.label1);
+            this.grBox_ThongKe.Location = new System.Drawing.Point(448, 36);
+            this.grBox_ThongKe.Name = "grBox_ThongKe";
+            this.grBox_ThongKe.Size = new System.Drawing.Size(340, 285);
+            this.grBox_ThongKe.TabIndex = 0;
+            this.grBox_ThongKe.TabStop = false;
+            this.grBox_ThongKe.Text = "Thống kê :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chi Nhánh :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(148, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(280, 285);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // trView_ChiNhanh
+            // 
+            this.trView_ChiNhanh.Location = new System.Drawing.Point(13, 36);
+            this.trView_ChiNhanh.Name = "trView_ChiNhanh";
+            this.trView_ChiNhanh.Size = new System.Drawing.Size(121, 150);
+            this.trView_ChiNhanh.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tổng doanh thu :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tổng đơn hàng:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(128, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(194, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // ThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trView_ChiNhanh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.grBox_ThongKe);
+            this.Name = "ThongKe";
+            this.Text = "ThongKe";
+            ((System.ComponentModel.ISupportInitialize)(this.do_an_phat_trien_phan_mem_udDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doanphattrienphanmemudDataSetBindingSource)).EndInit();
+            this.grBox_ThongKe.ResumeLayout(false);
+            this.grBox_ThongKe.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private do_an_phat_trien_phan_mem_udDataSet do_an_phat_trien_phan_mem_udDataSet;
+        private System.Windows.Forms.BindingSource doanphattrienphanmemudDataSetBindingSource;
+        private System.Windows.Forms.GroupBox grBox_ThongKe;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView trView_ChiNhanh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
